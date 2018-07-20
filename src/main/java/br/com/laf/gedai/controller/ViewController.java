4d89b2143f3f961d0c849393f33e55df6e6f3aa6
Daniel Soares <daniel.soares@zup.com.br>
2018-07-20 14:32:46 -0300

    First

--- src/main/java/br/com/laf/gedai/controller/ViewController.java
+++ src/main/java/br/com/laf/gedai/controller/ViewController.java
@@ -3,14 +3,14 @@ package br.com.laf.gedai.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.laf.gedai.model.Unidade;
 import br.com.laf.gedai.service.UnidadeService;
 
-@Controller
+@RestController
 public class ViewController {
 	
 	@Autowired
