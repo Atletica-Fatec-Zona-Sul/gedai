07e15d7d550c778b5a20d999cbd5781c54615b2b
Daniel Soares <daniel.soares@zup.com.br>
2018-07-20 13:34:48 -0300

    First commit

--- src/main/java/br/com/laf/gedai/GedaiApplication.java
+++ src/main/java/br/com/laf/gedai/GedaiApplication.java
@@ -9,11 +9,15 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class GedaiApplication extends SpringBootServletInitializer {
 
