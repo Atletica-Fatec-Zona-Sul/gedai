cd3cd9a430e4f0b444dc199503313a97ccfe551b
Daniel Soares <daniel.soares@zup.com.br>
2018-07-20 14:35:08 -0300

    First

--- src/main/java/br/com/laf/gedai/GedaiApplication.java
+++ src/main/java/br/com/laf/gedai/GedaiApplication.java
@@ -1,15 +1,11 @@
 package br.com.laf.gedai;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootConfiguration
-@EnableAutoConfiguration
 @SpringBootApplication
-public class GedaiApplication extends SpringBootServletInitializer {
+public class GedaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GedaiApplication.class, args);
