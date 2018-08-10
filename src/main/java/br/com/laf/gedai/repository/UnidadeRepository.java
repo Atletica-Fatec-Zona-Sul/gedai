5b66cd0cd19d2af6afb7dcf6424ebbfb8f17018d
Daniel Soares <daniel.soares@zup.com.br>
2018-08-10 15:52:28 -0300

    pra funcionar o deploy.

--- src/main/java/br/com/laf/gedai/repository/UnidadeRepository.java
+++ src/main/java/br/com/laf/gedai/repository/UnidadeRepository.java
@@ -1,11 +1,11 @@
-package br.com.laf.gedai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.laf.gedai.model.Unidade;
-
-@Repository
-public interface UnidadeRepository extends JpaRepository<Unidade, Integer> {
-
-}
+//package br.com.laf.gedai.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import br.com.laf.gedai.model.Unidade;
+//
+//@Repository
+//public interface UnidadeRepository extends JpaRepository<Unidade, Integer> {
+//
+//}
